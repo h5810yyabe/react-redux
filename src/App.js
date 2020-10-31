5094c8457f25226b4226ea923271a231e998613b
Yoshito Yabe <home@YoshitonoMacBook-puro.local>
2020-10-31 22:25:27 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 //JSXの構文。出力されるものをHTMLで直感的にかける
 function App() {
+  const greeting = "Hi!!";
+  const dom = <h1 className="foo">{greeting}</h1>;
   return (
-    <h1>
-      Hello, world!
-    </h1>
-  );
+    <React.Fragment>
+   
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={()=>{console.log("clicked")}} />
+    
+    </React.Fragment>
+  )
 }
 
 //JSの構文 JSXの構文がこれに自動変換される
