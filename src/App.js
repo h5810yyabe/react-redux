b5d39fdfddf4af0e0c5498bdbab77455ea0cc061
Yoshito Yabe <home@YoshitonoMacBook-puro.local>
2020-10-31 22:45:17 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, {Component} from 'react';
-//JSXの構文。出力されるものをHTMLで直感的にかける
+import React from 'react';
+//functional component
 function App() {
   const greeting = "Hi!!";
   const dom = <h1 className="foo">{greeting}</h1>;
   return (
-    <React.Fragment>
-   
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={()=>{console.log("clicked")}} />
-    
-    </React.Fragment>
+    <Cat></Cat>
   )
 }
 
-//JSの構文 JSXの構文がこれに自動変換される
-// function App() {
-//   return React.createElement(
-//     "div",
-//     null,
-//     "Hello, world!"
-//   );
+const Cat = () =>{
+  return <div>Meow!</div>;
+}
+
+//class component
+// class App extends Component{
+//   render(){
+//     return(<div>hello world</div>)
+//   }
 // }
+
 
 export default App;
