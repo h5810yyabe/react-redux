aaafd01091094d8cf2577563beb35cc3b46a06b3
Yoshito Yabe <home@YoshitonoMacBook-puro.local>
2020-10-31 22:08:04 +0900

    change App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import React, {Component} from 'react';
+//JSXの構文。出力されるものをHTMLで直感的にかける
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <h1>
+      Hello, world!
+    </h1>
   );
 }
+
+//JSの構文 JSXの構文がこれに自動変換される
+// function App() {
+//   return React.createElement(
+//     "div",
+//     null,
+//     "Hello, world!"
+//   );
+// }
 
 export default App;
