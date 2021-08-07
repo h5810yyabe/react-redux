2c4fc7f34aa7862cc719dd9aa2450ffc49d8443f
h5810yyabe <h5810yyabe@gmail.com>
2021-08-07 14:31:23 +0900

    stateを作成

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+
 //functional component
-function App() {
-  const profiles = [
-    {
-      name: "Taro",age:10
-    },
-    {
-      name: "Hanako",age:5
-    },
-    {
-      name: 1
-    }
-  ];
-  return (
-    <div>
-      {
-        profiles.map((profile,index) => {
-          return <User name={profile.name} age={profile.age} key={index} />;
-        })
-      }
-    </div>
-  );
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) =>{
-return <div>I am {props.name}. and {props.age} years old.</div>;
-}
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
 
+  handlePlusButton = () => {
+    //setStateを実行すると、stateが実行されたあとに差分のrenderが実行される
+    this.setState({count: this.state.count+1})
+  }
+
+  handleMinusButton = () => {
+    this.setState({count: this.state.count-1})
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
+}
 
 export default App;
