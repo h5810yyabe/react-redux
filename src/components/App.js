9928399ada199f28360066d1a54c649de5201114
h5810yyabe <h5810yyabe@gmail.com>
2021-08-28 11:07:32 +0900

    implement component with connect function

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,53 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
 
+import { increment, decrement } from "../actions";
 
 //functional component
-const App = () => (<Counter></Counter>)
+class App extends Component {
+  //reducerでintialstateの設定されてるので不要
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {count: 0}
+  // }
 
+  //reducerの中でcountの増減処理するので不要
+  // handlePlusButton = () => {
+  //   //setStateを実行すると、stateが実行されたあとに差分のrenderが実行される
+  //   this.setState({count: this.state.count+1})
+  // }
 
-class Counter extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {count: 0}
-  }
+  // handleMinusButton = () => {
+  //   this.setState({count: this.state.count-1})
+  // }
 
-  handlePlusButton = () => {
-    //setStateを実行すると、stateが実行されたあとに差分のrenderが実行される
-    this.setState({count: this.state.count+1})
-  }
-
-  handleMinusButton = () => {
-    this.setState({count: this.state.count-1})
-  }
-
+  //onClickのタイミングでprops.increment実行
+  //ｰ>props.incrementはmapDispatchToStateによりdispatch(increment())がセットされてる
+  //->dispatch(increment())が実行。reducerが実行される
+  //
   render() {
+    const props = this.props;
+
     return (
       <React.Fragment>
-        <div>count: {this.state.count}</div>
-        <button onClick={this.handlePlusButton}>+1</button>
-        <button onClick={this.handleMinusButton}>-1</button>
+        <div>count: {props.value}</div>
+        <button onClick={props.increment}>+1</button>
+        <button onClick={props.decrement}>-1</button>
       </React.Fragment>
     )
   }
 }
 
-export default App;
+const mapStateToProps = state => ({value: state.count.value})
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement())
+// })
+
+const mapDispatchToProps = ({increment, decrement})
+
+//1.connect関数によりmapStateToPropsが実行ｰ>AppのPropsにstateが入る
+//2.connect関数によりmapDispatchToPropsが実行ｰ>props.increment,decrementが設定される
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
